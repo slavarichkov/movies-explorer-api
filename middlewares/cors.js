33e28a54f6ada777b86053b8170da9a8dd27463b
Slava Richkov <v.v.richkov@yandex.ru>
2023-03-23 20:30:27 +0800

    feat: cors

---
+++ middlewares/cors.js
@@ -0,0 +1,40 @@
+// Массив доменов, с которых разрешены кросс-доменные запросы
+const allowedCors = [
+  'https://praktikum.tk',
+  'http://praktikum.tk',
+  'localhost:3000',
+];
+
+function corsSimple(req, res, next) { // простые CORS-запросы
+  const { origin } = req.headers; // Сохраняем источник запроса в переменную origin
+  // проверяем, что источник запроса есть среди разрешённых
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+  }
+  next();
+}
+
+function corsMultiPart(req, res, next) { // сложные CORS-запросы
+  const { method } = req; // Сохраняем тип запроса (HTTP-метод) в соответствующую переменную
+  // Значение для заголовка Access-Control-Allow-Methods по умолчанию (разрешены все типы запросов)
+  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+  // Если это предварительный запрос, добавляем нужные заголовки
+  if (method === 'OPTIONS') {
+    // разрешаем кросс-доменные запросы любых типов (по умолчанию)
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+  }
+  // сохраняем список заголовков исходного запроса
+  const requestHeaders = req.headers['access-control-request-headers'];
+  if (method === 'OPTIONS') {
+    // разрешаем кросс-доменные запросы с этими заголовками
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+    // завершаем обработку запроса и возвращаем результат клиенту
+    return res.end();
+  }
+  return next();
+}
+
+module.exports = {
+  corsSimple,
+  corsMultiPart,
+};
